99c3da49c5c49c70a902a45b6ea34e6c8ca96724
Saket <samsaket7@gmail.com>
2021-02-27 14:18:10 +0000

    example1.c

--- apps/examples/example1/.solution/refactored/example1.c
+++ apps/examples/example1/.solution/refactored/example1.c
@@ -16,7 +16,10 @@
      "guarddirective": { "operation": "allow"}, \
      "argtaints": [], \
      "codtaints": ["ORANGE"], \
-     "rettaints": ["TAG_RESPONSE_GET_A"] \
+     "rettaints": ["TAG_RESPONSE_GET_A"], \
+     "idempotent": true, \
+     "checkpointable": true, \
+     "lossResistance": "medium" \
     } \
   ] }
 
