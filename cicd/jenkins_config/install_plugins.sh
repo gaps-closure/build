98f7349111a48f0fe90d696b70a3c20b22e8d283
Andrzej Cichocki <acichocki@perspectalabs.com>
2021-04-30 20:53:51 +0000

    Added script to install necessary plugins for Jenkins

---
+++ cicd/jenkins_config/install_plugins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PASSWD=${1:-admin}
+
+JENKINS_CLI=jenkins-cli.jar
+JENKINS_URL=http://localhost:8080
+JENKINS_AUTH=admin:$PASSWD
+
+if [ -f "$JENKINS_CLI" ]
+then
+  echo "Using $JENKINS_CLI"
+else
+  wget -O "$JENKINS_CLI" $JENKINS_URL/jnlpJars/jenkins-cli.jar
+fi
+
+while read -u 10 p || [ -n "$p" ]
+do
+  CMD="java -jar $JENKINS_CLI -s $JENKINS_URL -auth $JENKINS_AUTH install-plugin $p"
+  echo $CMD
+  $CMD
+done 10<"plugins.txt"
+
+
